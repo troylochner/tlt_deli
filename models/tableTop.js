4ba69cfa5e587b2a67406d879bc546f942bb935b
troylochner <troy.lochner@gmail.com>
2020-11-09 21:19:40 -0500

    Order & Table Top api endpoints done

--- models/tableTop.js
+++ models/tableTop.js
@@ -6,7 +6,6 @@ module.exports = function(sequelize, DataTypes) {
     },
     tableStatus: {
       type: DataTypes.BOOLEAN,
-      allowNull: false,
       default: false
     }
   });
--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.use(passport.session());
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 require("./routes/menu-api-routes.js")(app);
+require("./routes/tableTop-api-routes.js")(app);
+require("./routes/order-api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync({ alter: true }).then(() => {
