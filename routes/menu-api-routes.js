375f42018043c7a0f30e8b166dd72646ee34ac6a
troylochner <troy.lochner@gmail.com>
2020-11-09 20:48:56 -0500

    CRUD working for menu items

--- routes/menu-api-routes.js
+++ routes/menu-api-routes.js
@@ -2,12 +2,39 @@
 const db = require("../models");
 
 module.exports = function(app) {
-  app.get("/api/fullmenu", (req, res) => {
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
-    db.menuItem.findAll({}).then(menuItems => {
-      res.json(menuItems);
+  app.get("/api/menuitems", (req, res) => {
+    db.menuItem.findAll({}).then(items => {
+      res.json(items);
     });
+  });
+
+  app.get("/api/menuitems/:id", (req, res) => {
+    db.menuItem
+      .findOne({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(item => {
+        res.json(item);
+      });
+  });
+
+  app.post("/api/menuitems", (req, res) => {
+    db.menuItem.create(req.body).then(item => {
+      res.json(item);
+    });
+  });
+
+  app.delete("/api/menuitems/:id", (req, res) => {
+    db.menuItem
+      .destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(item => {
+        res.json(item);
+      });
   });
 };
