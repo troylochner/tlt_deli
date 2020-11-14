795ce3f187f3aae942c093978df83677b253f112
troylochner <troy.lochner@gmail.com>
2020-11-14 13:20:07 -0500

    Merging all items to the master

--- models/orderMenuItem.js
+++ models/orderMenuItem.js
@@ -1,38 +1,24 @@
-const menuItem = require("./menuItem.js");
-const order = require("./order.js");
+//const menuItem = require("./menuItem.js");
+//const order = require("./order.js");
 
 module.exports = function(sequelize, DataTypes) {
   const orderMenuItem = sequelize.define("orderMenuItem", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true,
-      unique: true
-    },
     menuItemId: {
       type: DataTypes.INTEGER,
-      //unique: false,
+      allowNull: false,
       references: {
-        model: menuItem,
+        model: "menuItem",
         key: "id"
       }
     },
     orderId: {
       type: DataTypes.INTEGER,
-      //unique: false,
+      allowNull: false,
       references: {
-        model: order,
+        model: "order",
         key: "id"
       }
     }
   });
-  orderMenuItem.associate = function(models) {
-    orderMenuItem.belongsTo(models.order, {
-      through: models.order,
-      unique: false
-    });
-  };
-
   return orderMenuItem;
 };
