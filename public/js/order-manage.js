e3cf99034cd675d60d63a362681275031a7e3aeb
leo-ronan <57088324+leo-ronan@users.noreply.github.com>
2020-11-16 18:53:10 -0500

    Update order-manage.js
    Added documentation

--- public/js/order-manage.js
+++ public/js/order-manage.js
@@ -1,16 +1,17 @@
 $(document).ready(() => {
-  const placeOrder = $("button.order");
-  const orderedItems = $("placeholder");
+  const placeOrder = $("button.order"); //SUBJECT TO CHANGE
+  const orderedItems = $("placeholder"); //SUBJECT TO CHANGE
   const newOrderId = "";
   const orderPath = "api/orders/" + newOrderId;
   const email = "";
   const name = "";
-  //Retrieve values from html (orderData) and runs function addOrder
+  //Retrieve values from html (orderData) and runs function addOrder (RUNS ON username and email submit on orderForm.html)
   placeOrder.on("submit", event => {
     event.preventDefault();
     const orderData = {
-      items: orderedItems.val().trim()
+      items: orderedItems.val().trim() //SUBJECT TO CHANGE (items in order may be located elsewhere)
     };
+    // It's possible we can just take the data from the email/user inputs if we scrap the login/members system that we had initially
     $.get("/api/user_data").then(data => {
       email = data.email;
       name = data.name;
@@ -21,14 +22,15 @@ $(document).ready(() => {
     }).then(() => {
       newOrderId = this.id;
     });
+    // addOrder function may have to be connected to a different element
     addOrder(orderData, newOrderId);
-    orderTotal();
+    orderTotal(); //LOCATION OF FUNCTION CALL SUBJECT TO CHANGE
     orderItems.val("");
   });
 
   // Adds items to order and posts order
   function addOrder(order, orderId) {
-    //Update orderPath (/api/orders/<current id>)
+    // Update orderPath (/api/orders/<current id>)
     orderPath = "api/orders/" + orderId;
     $.post(orderPath + "/add", {
       menuItem: id
@@ -44,7 +46,7 @@ $(document).ready(() => {
         }
       })
       .then(() => {
-        //Do we need a new price (total) column?
+        //UNFINISHED BELOW
         $.post(orderPath, {
           items: price
         });
