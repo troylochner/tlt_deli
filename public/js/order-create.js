a919a35aa6678cb1aca3c900bcf667c5f840f6f0
leo-ronan <57088324+leo-ronan@users.noreply.github.com>
2020-11-21 14:16:11 -0500

    Update order-create.js

--- public/js/order-create.js
+++ public/js/order-create.js
@@ -61,54 +61,5 @@ $(document).ready(() => {
           });
         });
       });
-=======
-  const newOrderId = "";
-  // Create a new order
-  $(() => {
-    $(".create-form").on("submit", event => {
-      event.preventDefault();
-
-      const order = {
-        custName: $("#custName")
-          .val()
-          .trim(),
-        email: $("#custEmail")
-          .val()
-          .trim()
-      };
-
-      // Send the POST request.
-      $.ajax("/api/orders", {
-        type: "POST",
-        data: order
-      }).then(() => {
-        console.log(order);
-        newOrderId = this.id;
-      });
-    });
-  }).then(() => {
-    const orderItems = {
-      menuItemId: $("#placeholderId")
-        .val()
-        .trim(),
-      qty: $("#placeholderQty")
-        .val()
-        .trim(),
-      itemName: $("#placeholderName")
-        .val()
-        .trim(),
-      price: $("#placeholderPrice")
-        .val()
-        .trim()
-    };
-
-    $.ajax("/api/orders/" + newOrderId + "/add/", {
-      type: "POST",
-      data: orderItems
-    }).then(() => {
-      console.log(orderItems);
-      window.location.replace("orders.handlebars");
-    });
-
   });
 });
